f5dc43e3f2482222fd35f964e7cb00642f56d74e
Ricardo Guerra <RikkuG491@gmail.com>
2024-04-25 10:52:17 -0400

    Removed solution code from index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 // Write your code here
-let username = "alice123"
-let password = "flatironschool"
-const loginValidationPhrase = username === "alice123" && password === "flatironschool" ? "Welcome alice123!" : "Invalid username or password! Please try again!"
-const randomNumber = Math.ceil(Math.random() * 10)
-const luckyNumberPhrase = `Your lucky number is ${randomNumber}!`
